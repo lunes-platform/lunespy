ff23bedd48e47fdb64f5d40f0cc3921f849d8f1d
Marcos Lima <marcoslkz@gmail.com>
2018-07-24 14:29:34 +0000

    Clean old urls

---
+++ order.py
@@ -0,0 +1,31 @@
+import LunesPy as lunespy
+
+class Order(object):
+    def __init__(self, orderId, assetPair, address = ''):
+        self.orderId = orderId
+        self.assetPair = assetPair
+        self.address = address
+        self.matcher = lunespy.MATCHER
+        self.matcherPublicKey = lunespy.MATCHER_PUBLICKEY
+        self.status()
+
+    def __str__(self):
+        return 'status = %s\n' \
+               'id = %s\n' \
+               '%s\n' \
+               'sender.address = %s\n' \
+               'sender.publicKey = %s\n' \
+               'matcher = %s' % (self.status(), self.orderId, self.assetPair, self.address.address, self.address.publicKey, self.matcherPublicKey)
+
+    def status(self):
+        try:
+            req = lunespy.wrapper('/matcher/orderbook/%s/%s/%s' % ('LUNES' if self.assetPair.asset1.assetId=='' else self.assetPair.asset1.assetId, 'LUNES' if self.assetPair.asset2.assetId=='' else self.assetPair.asset2.assetId, self.orderId), host=self.matcher)
+            return req['status']
+        except:
+            pass
+
+    def cancel(self):
+        if self.address:
+            self.address.cancelOrder(self.assetPair, self)
+
+    __repr__ = __str__
