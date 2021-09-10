0709358ad3e8e66194bb3aae0f2bebde6353e9e4
olivmath <lucas.oliveira@lunes.io>
2021-09-10 01:46:21 -0300

    added: client actions
    in `client` are any constants and functions to interact with nodes
    iterations are treated as `transactions`:
    - and there are several types of them

--- lunespy/__init__.py
+++ lunespy/__init__.py
@@ -1,21 +1,3 @@
-# Copyright (C) 2017 LunesPy Developers
-#
-# This file is part of LunesPy.
-#
-# It is subject to the license terms in the LICENSE file found in the top-level
-# directory of this distribution.
-#
-# No part of python-bitcoinlib, including this file, may be copied, modified,
-# propagated, or distributed except according to the terms contained in the
-# LICENSE file.
-
-from __future__ import absolute_import, division, print_function, unicode_literals
-
-
-logging.getLogger("requests").setLevel(logging.WARNING)
-console = logging.StreamHandler()
-console.setLevel(logging.ERROR)
-formatter = logging.Formatter('[%(levelname)s] %(message)s')
-console.setFormatter(formatter)
-logging.getLogger('').addHandler(console)
-
+from lunespy.client import *
+from lunespy.server import *
+from lunespy.utils import * 
