aa396658b3e147c446cfad1b834a79836344fb63
Lucas Oliveira <olivmath@protonmail.com>
2021-09-12 02:41:25 -0300

    changed: refactor chain
    the `chain_id` is now owned by `Account`
    this way the creation of mainnet or testnet wallets is more explicit:
    - being chosen by the user
    
    refactoring the names:
    - `privateKey` and `publicKey` to `private_key` and `public_key`
    
    There was also a big change in the logic of the wallet generator:
    - the execution and responsibility of generating the data was changed:
    each function responsible for a data:
    - `seed_generator` by `seed` and so on...

--- lunespy/client/wallet/validators.py
+++ lunespy/client/wallet/validators.py
@@ -4,36 +4,40 @@ from lunespy.client.wallet.errors import InvalidLengthAddress
 from lunespy.client.wallet.errors import InvalidChainAddress
 from lunespy.client.wallet.errors import InvalidNonce
 from lunespy.client.wallet.generators import wallet_generator
-from lunespy.client.wallet import ADDRESS_CHECKSUM_LENGTH
-from lunespy.client.wallet import ADDRESS_VERSION
-from lunespy.client.wallet import ADDRESS_LENGTH
+from lunespy.client.wallet.constants import ADDRESS_CHECKSUM_LENGTH
+from lunespy.client.wallet.constants import ADDRESS_VERSION
+from lunespy.client.wallet.constants import ADDRESS_LENGTH
 from lunespy.utils.crypto.converters import bytes_to_string
 from lunespy.utils.crypto.converters import string_to_bytes
 from lunespy.utils.crypto.converters import hash_chain
-from lunespy.server import CHAIN_ID
 from lunespy.server import OFFLINE
 from lunespy.server.address import aliases
 
 def validate_wallet(wallet: dict) -> dict:
     wallet['nonce'] = wallet.get('nonce', 0)
     
+    if wallet.get('chain', 'mainnet') == 'mainnet':
+        chain_id: str = '1'
+    elif wallet['chain'] == 'testnet':
+        chain_id: str = '0'
+    
     if wallet['nonce'] not in range(0, 4294967295 + 1):
         raise InvalidNonce
 
     elif wallet.get('seed', False):
-        return wallet_generator(seed=wallet['seed'], nonce=wallet['nonce'])
+        return wallet_generator(seed=wallet['seed'], nonce=wallet['nonce'], chain_id=chain_id)
     
-    elif wallet.get('privateKey', False):
-        return wallet_generator(privateKey=wallet['privateKey'])
+    elif wallet.get('private_key', False):
+        return wallet_generator(private_key=wallet['private_key'], nonce=wallet['nonce'], chain_id=chain_id)
     
-    elif wallet.get('publicKey', False):
-        return wallet_generator(publicKey=wallet['publicKey'])
+    elif wallet.get('public_key', False):
+        return wallet_generator(public_key=wallet['public_key'], nonce=wallet['nonce'], chain_id=chain_id)
     
     elif wallet.get('address', False):
-        if validate_address(wallet['address']):
+        if validate_address(wallet['address'], chain_id=chain_id):
             return {
-                'privateKey': '',
-                'publicKey': '',
+                'private_key': '',
+                'public_key': '',
                 'address': wallet['address'],
                 'nonce': 0,
                 'seed': ''
@@ -42,28 +46,29 @@ def validate_wallet(wallet: dict) -> dict:
     elif wallet.get('alias') and not OFFLINE:
         return {
             'address': aliases(wallet['alias']),
-            'publicKey': '',
-            'privateKey': '',
+            'public_key': '',
+            'private_key': '',
             'seed': '',
             'nonce': 0,
         }
     
     else:
-        return wallet_generator(nonce=wallet['nonce'])
+        return wallet_generator(nonce=wallet['nonce'], chain_id=chain_id)
 
 
-def validate_address(address: str) -> bool:
-    bytes_address = bytes_to_string(address)
+def validate_address(address: str, chain_id: str) -> bool:
+    bytes_address = bytes_to_string(address, decode=True)
     checksum = bytes_address[-ADDRESS_CHECKSUM_LENGTH:]
     chain = hash_chain(string_to_bytes(
         bytes_address[:-ADDRESS_CHECKSUM_LENGTH]
         )
     )[:ADDRESS_CHECKSUM_LENGTH]
+    
     if checksum != chain:
         raise InvalidChecksumAddress
     elif bytes_address[0] != chr(ADDRESS_VERSION):
         raise InvalidVersionAddress
-    elif bytes_address[1] != CHAIN_ID:
+    elif bytes_address[1] != chain_id:
         raise InvalidChainAddress
     elif len(bytes_address) != ADDRESS_LENGTH:
         raise InvalidLengthAddress
