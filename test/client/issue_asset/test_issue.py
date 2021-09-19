0832621887e026df8de880e5ed4be5136161b3b0
Lucas Oliveira <olivmath@protonmail.com>
2021-09-19 17:13:58 -0300

    fixed: quantity cannot be less than 0
    fix quantity cannot be less than 0 for validade issue_data
    add test for IssueAsset
    add test for TransferAsset

---
+++ test/client/issue_asset/test_issue.py
@@ -0,0 +1,65 @@
+from lunespy.client.transactions.issue_asset import IssueAsset
+from lunespy.client.wallet import Account
+
+def test_ready_failed_name():
+    """
+        without a name parameter.
+        should be return False for IssueAsset.ready
+    """
+    # Failed
+    sender = Account()
+    tx = IssueAsset(sender, quantity=1)
+    assert tx.ready == False
+
+def test_ready_failed_quantity():
+    """
+        without a quantity parameter.
+        should be return False for IssueAsset.ready
+    """
+    # Failed
+    sender = Account()
+    tx = IssueAsset(sender, name='test')
+    assert tx.ready == False
+
+
+def test_ready_success():
+    """
+        with a sender, name and quantity.
+        should be return True for IssueAsset.ready
+    """    
+    # Success
+    sender = Account()
+    tx = IssueAsset(sender, name='test', quantity=10)
+    assert tx.ready == True
+
+
+def test_transaction_full_data():
+    """
+        with a sender, receiver, amount.
+        should be return all keys of offline-transaction for IssueAsset.transaction  
+    """
+    sender = Account()
+    offline_transaction = [
+        'ready',
+        'senderPublicKey',
+        'signature',
+        'description',
+        'reissuable',
+        'timestamp',
+        'type',
+        'decimals',
+        'quantity',
+        'fee',
+        'name',
+        'feeAsset',
+        'assetId'
+    ]
+
+    tx = IssueAsset(sender, name='test', quantity=10)
+    response = tx.transaction
+    print(response)
+
+    assert response['ready'] == True
+
+    for i, j in zip(offline_transaction, response.keys()):
+        assert i == j
---
+++ test/client/transfer_assets/test_transfer.py
@@ -0,0 +1,51 @@
+from lunespy.client.transactions.transfer_asset import TransferAsset
+from lunespy.client.wallet import Account
+
+def test_ready_failed():
+    """
+        without a amount parameter.
+        should be return False for TransaferAsset.ready
+    """
+    # Failed
+    sender = Account()
+    receiver = Account()
+    tx = TransferAsset(sender, receiver)
+    assert tx.ready == False
+
+def test_ready_success():
+    """
+        with a sender, receiver and amount.
+        should be return True for TransaferAsset.ready
+    """    
+    # Success
+    sender = Account()
+    receiver = Account()
+    tx = TransferAsset(sender, receiver, amount=10)
+    assert tx.ready == True
+
+
+def test_transaction_full_data():
+    """
+        with a sender, receiver, amount.
+        should be return all keys of offline-transaction for TransaferAsset.transaction  
+    """
+    sender = Account()
+    receiver = Account()
+    offline_transaction = [
+        'ready',
+        'senderPublicKey',
+        'signature',
+        'timestamp',
+        'recipient',
+        'feeAsset',
+        'assetId',
+        'amount',
+        'type',
+        'fee']
+    tx = TransferAsset(sender, receiver, amount=10)
+    response = tx.transaction
+
+    assert response['ready'] == True
+
+    for i, j in zip(offline_transaction, response.keys()):
+        assert i == j
