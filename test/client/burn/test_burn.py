04baab3965dba159e37225c39b087586bdc464aa
olivmath <lucas.oliveira@lunes.io>
2021-09-24 02:41:25 -0300

    removed: test requests

--- test/client/burn/test_burn.py
+++ test/client/burn/test_burn.py
@@ -43,12 +43,3 @@ def test_transaction_full_data():
 
     for i, j in zip(offline_transaction, response.keys()):
         assert i == j
-
-
-# todo a mock
-def test_send_failed_successful():
-    """
-        should be return False for `send` parameter and dict in `response`
-        should be return True for `send` parameter and dict in `response`
-    """
-    assert 1 == 1
