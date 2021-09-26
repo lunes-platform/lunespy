93923c22188a9f2ad3303e13811d29aaa44f207c
olivmath <lucas.oliveira@lunes.io>
2021-09-26 08:44:03 -0300

    fixed: add again lease

---
+++ lunespy/client/transactions/lease/__init__.py
@@ -0,0 +1,40 @@
+from lunespy.client.transactions import BaseTransaction
+from lunespy.client.wallet import Account
+from lunespy.client.transactions.lease.validators import validate_lease
+from lunespy.client.transactions.lease.validators import mount_lease
+from lunespy.client.transactions.lease.validators import send_lease
+from lunespy.server import NODE_URL
+
+class CreateLease(BaseTransaction):
+    """
+    lease_data: dict
+        validator_address: str
+        lease_fee: int
+        timestamp: int
+        amount: int
+    """
+    def __init__(self, staker: Account, validator_address: str, **lease_data: dict) -> None:
+        super().__init__('Create Lease', lease_data)
+        self.staker: Account = staker
+        self.validator_address: str = validator_address
+        self.lease_data: dict = lease_data
+        self.history: list = []
+
+    
+    @property
+    def ready(self) -> bool:
+        return validate_lease(self.staker, self.lease_data)
+
+
+    @property
+    def transaction(self) -> dict:
+        return super().transaction(
+            mount_tx=mount_lease,
+            staker=self.staker,
+            validator_address=self.validator_address,
+            lease_data=self.lease_data)
+
+    def send(self, node_url_address: str=NODE_URL) -> dict:
+        tx = super().send(send_lease, node_url_address)
+        self.history.append(tx)
+        return tx
---
+++ lunespy/client/transactions/lease/validators.py
@@ -0,0 +1,69 @@
+from lunespy.client.transactions.lease.constants import DEFAULT_CREATE_LEASE_FEE
+from lunespy.client.transactions.lease.constants import BYTE_TYPE_CREATE_LEASE
+from lunespy.client.transactions.lease.constants import INT_TYPE_CREATE_LEASE
+from lunespy.utils.crypto.converters import sign
+from lunespy.utils.settings import bcolors
+from lunespy.client.wallet import Account
+from lunespy.server import NODE_URL
+from datetime import datetime
+from base58 import b58decode
+from requests import post
+import struct
+
+
+def mount_lease(staker: Account, validator_address: str, lease_data: dict) -> dict:
+    timestamp: int = lease_data.get('timestamp', int(datetime.now().timestamp() * 1000))
+    amount: int = lease_data['amount']
+    lease_fee: int = lease_data.get('lease_fee', DEFAULT_CREATE_LEASE_FEE)    
+
+    bytes_data: bytes = BYTE_TYPE_CREATE_LEASE + \
+        b58decode(staker.public_key) + \
+        b58decode(validator_address) + \
+        struct.pack(">Q", amount) + \
+        struct.pack(">Q", lease_fee) + \
+        struct.pack(">Q", timestamp)
+
+    signature: bytes = sign(staker.private_key, bytes_data)
+    mount_tx: dict = {
+        "type": INT_TYPE_CREATE_LEASE,
+        "senderPublicKey": staker.public_key,
+        "signature": signature.decode(),
+        "timestamp": timestamp,
+        "fee": lease_fee,
+
+        "recipient": validator_address,
+        "amount": amount
+    }
+    return mount_tx
+
+
+def validate_lease(staker: Account, lease_data: dict) -> bool:
+    amount: int = lease_data.get('amount', -1)
+
+    if not staker.private_key:
+        print(bcolors.FAIL + 'Staker `Account` not have a private key' + bcolors.ENDC)
+        return False
+    elif amount <= 0:
+        print(bcolors.FAIL + 'Leasing `amount` cannot be less than 0' + bcolors.ENDC)
+        return False
+    return True
+
+
+# todo async
+def send_lease(mount_tx: dict, node: str) -> dict:
+    response = post(
+        f'{node}/transactions/broadcast',
+        json=mount_tx,
+        headers={
+            'content-type':
+            'application/json'
+        })
+
+    if response.ok:
+        mount_tx['send'] = True
+        mount_tx['response'] = response.json()
+        return mount_tx
+    else:
+        mount_tx['send'] = False
+        mount_tx['response'] = response.text
+        return mount_tx
