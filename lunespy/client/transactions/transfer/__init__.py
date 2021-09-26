e2b04e3c06a6ec2c4c827eb9539a37439b50993b
olivmath <lucas.oliveira@lunes.io>
2021-09-26 04:06:52 -0300

    added: remodeled transaction base class
    move all operational code for `BaseTransaction`
    add log for successful or failed transaction
    add function for export transaction as .json
    rename 'tx_data' to 'transfer_data'

--- lunespy/client/transactions/transfer/__init__.py
+++ lunespy/client/transactions/transfer/__init__.py
@@ -4,18 +4,19 @@ from lunespy.client.transactions.transfer.validators import send_transfer
 from lunespy.client.transactions import BaseTransaction
 from lunespy.client.wallet import Account
 from lunespy.utils.settings import bcolors
-
+from lunespy.server import NODE_URL
 
 class TransferToken(BaseTransaction):
     """
     transfer_data: dict
-        @params fee_asset: int
-        @params timestamp: int
-        @params asset_id: str
-        @params tx_fee: int
-        @params amount: int
+        transfer_fee: int
+        timestamp: int
+        asset_fee: int
+        asset_id: str
+        amount: int
     """
-    def __init__(self, sender: Account, receiver: Account, **transfer_data) -> None:
+    def __init__(self, sender: Account, receiver: Account, **transfer_data: dict) -> None:
+        super().__init__('Transfer', transfer_data)
         self.sender: Account = sender
         self.receiver: Account = receiver
         self.transfer_data: dict = transfer_data
@@ -30,25 +31,15 @@ class TransferToken(BaseTransaction):
 
     @property
     def transaction(self) -> dict:
-        if self.ready:
-            mount_tx = {'ready': True}
-            mount_tx.update(mount_transfer(
-                self.sender,
-                self.receiver,
-                self.transfer_data
-                )
-            )
-            return mount_tx
-        else:
-            print(bcolors.FAIL + 'Transfer Transactions bad formed', bcolors.ENDC)
-            return {'ready': False}
-    @property
-    def send(self) -> dict:
-        mounted_tx = self.transaction
-        if mounted_tx['ready']:
-            tx_history = send_transfer(mounted_tx)
-            self.history.append(tx_history)
-            return tx_history
-        else:
-            print(bcolors.FAIL + 'Transfer Transactions dont send', bcolors.ENDC)
-            return mounted_tx
+        return super().transaction(
+            mount_tx=mount_transfer,
+            sender=self.sender,
+            receiver=self.receiver,
+            transfer_data=self.transfer_data
+                
+        )
+
+    def send(self, node_url_address: str = NODE_URL) -> dict:
+        tx = super().send(send_transfer, node_url_address)
+        self.history.append(tx)
+        return tx
--- lunespy/client/transactions/transfer/validators.py
+++ lunespy/client/transactions/transfer/validators.py
@@ -4,45 +4,45 @@ from lunespy.client.transactions.transfer.constants import INT_TYPE_TRANSFER
 from lunespy.utils.crypto.converters import sign
 from lunespy.utils.settings import bcolors
 from lunespy.client.wallet import Account
-from lunespy.server import NODE_URL
 from datetime import datetime
 from base58 import b58decode
 from requests import post
 import struct
 
 
-def mount_transfer(sender: Account, receiver: Account, tx_data: dict) -> dict:
-    timestamp: int = tx_data.get('timestamp', int(datetime.now().timestamp() * 1000))
-    amount: int = tx_data['amount']
-    transfer_fee: int = tx_data.get('transfer_fee', DEFAULT_TRANSFER_FEE)
-    asset_id: str = tx_data.get('asset_id', "")
-    fee_asset: str = tx_data.get('fee_asset', "")
+def mount_transfer(sender: Account, receiver: Account, transfer_data: dict) -> dict:
+    timestamp: int = transfer_data.get('timestamp', int(datetime.now().timestamp() * 1000))
+    amount: int = transfer_data['amount']
+    transfer_fee: int = transfer_data.get('transfer_fee', DEFAULT_TRANSFER_FEE)
+    asset_id: str = transfer_data.get('asset_id', "")
+    asset_fee: str = transfer_data.get('asset_fee', "")
 
     bytes_data: bytes = BYTE_TYPE_TRANSFER + \
         b58decode(sender.public_key) + \
         (b'\1' + b58decode(asset_id) if asset_id != "" else b'\0') + \
-        (b'\1' + b58decode(fee_asset) if fee_asset != "" else b'\0') + \
+        (b'\1' + b58decode(asset_fee) if asset_fee != "" else b'\0') + \
         struct.pack(">Q", timestamp) + \
         struct.pack(">Q", amount) + \
         struct.pack(">Q", transfer_fee) + \
         b58decode(receiver.address)
     signature: bytes = sign(sender.private_key, bytes_data)
     mount_tx: dict = {
+        "type": INT_TYPE_TRANSFER,
         "senderPublicKey": sender.public_key,
         "signature": signature.decode(),
         "timestamp": timestamp,
+        "fee": transfer_fee,
+        
         "recipient": receiver.address,
-        "feeAsset": fee_asset,
+        "feeAsset": asset_fee,
         "assetId": asset_id,
         "amount": amount,
-        "type": INT_TYPE_TRANSFER,
-        "fee": transfer_fee
     }
     return mount_tx
 
 
-def validate_transfer(sender: Account, tx_data: dict) -> bool:
-    amount: int = tx_data.get('amount', -1)
+def validate_transfer(sender: Account, transfer_data: dict) -> bool:
+    amount: int = transfer_data.get('amount', -1)
 
     if not sender.private_key:
         print(bcolors.FAIL + 'Sender `Account` not have a private key' + bcolors.ENDC)
@@ -54,9 +54,9 @@ def validate_transfer(sender: Account, tx_data: dict) -> bool:
 
 
 # todo async
-def send_transfer(mount_tx: dict) -> dict:
+def send_transfer(mount_tx: dict, node_url_address: str) -> dict:
     response = post(
-        f'{NODE_URL}/transactions/broadcast',
+        f'{node_url_address}/transactions/broadcast',
         json=mount_tx,
         headers={
             'content-type':
@@ -69,5 +69,5 @@ def send_transfer(mount_tx: dict) -> dict:
         return mount_tx
     else:
         mount_tx['send'] = False
-        mount_tx['response'] = response.json()
+        mount_tx['response'] = response.text
         return mount_tx
