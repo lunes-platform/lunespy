9109def69fae11feaa310ca45edbaf572311b98c
xonfps <isaque.souza@lunes.io>
2021-11-07 16:13:07 -0300

    fixed: correct way

--- lunespy/server/blocks/__init__.py
+++ lunespy/server/blocks/__init__.py
@@ -1,7 +1,31 @@
 from requests import get 
 
-def block_from_height(height: str) -> dict:
-    full_url = f'https://lunesnode.lunes.io/blocks/at/{height}'
+def block_from_height(node: str, height: str) -> dict:
+    full_url = f'https://{node}/blocks/at/{height}'
     response = get(full_url)
-    return response.json()
     
+    if response.ok:
+        return {
+            'status': 'ok',
+            'response': response.json()
+        }
+    else:
+        return {
+            'status': 'error',
+            'response': response.text
+        }
+
+def range_block(node: str, start_block: str, end_block: str) -> dict:
+    full_url = f'https://{node}/blocks/seq/{start_block}/{end_block}'
+    response = get(full_url)
+    
+    if response.ok:
+        return {
+            'status': 'ok',
+            'response': response.json()
+        }
+    else:
+        return {
+            'status': 'error',
+            'response': response.text
+        }
