ff5da593b8d6601b16662b2fdf8356764f09a84f
xonfps <isaque.souza@lunes.io>
2021-11-08 10:45:12 -0300

    fixed: correct name for variable node

--- lunespy/server/blocks/__init__.py
+++ lunespy/server/blocks/__init__.py
@@ -1,7 +1,7 @@
 from requests import get 
 
-def block_from_height(node: str, height: str) -> dict:
-    full_url = f'https://{node}/blocks/at/{height}'
+def block_from_height(node_url: str, height: int) -> dict:
+    full_url = f'https://{node_url}/blocks/at/{height}'
     response = get(full_url)
     
     if response.ok:
@@ -15,8 +15,8 @@ def block_from_height(node: str, height: str) -> dict:
             'response': response.text
         }
 
-def range_block(node: str, start_block: str, end_block: str) -> dict:
-    full_url = f'https://{node}/blocks/seq/{start_block}/{end_block}'
+def range_block(node_url: str, start_block: int, end_block: int) -> dict:
+    full_url = f'https://{node_url}/blocks/seq/{start_block}/{end_block}'
     response = get(full_url)
     
     if response.ok:
