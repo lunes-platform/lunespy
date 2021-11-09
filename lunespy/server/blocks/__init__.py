37ba58febd6f69141b00d7bda9515c34b292583b
olivmath <lucas.oliveira@lunes.io>
2021-11-09 14:27:38 -0300

    fixed: function with no name

--- lunespy/server/blocks/__init__.py
+++ lunespy/server/blocks/__init__.py
@@ -1,10 +1,11 @@
 from requests import get 
 
 
+
 def block_from_height(node_url: str, height: int) -> dict:
     full_url = f'https://{node_url}/blocks/at/{height}'
     response = get(full_url)
-    
+
     if response.ok:
         return {
             'status': 'ok',
@@ -20,7 +21,7 @@ def block_from_height(node_url: str, height: int) -> dict:
 def range_block(node_url: str, start_block: int, end_block: int) -> dict:
     full_url = f'https://{node_url}/blocks/seq/{start_block}/{end_block}'
     response = get(full_url)
-    
+
     if response.ok:
         return {
             'status': 'ok',
@@ -33,6 +34,7 @@ def range_block(node_url: str, start_block: int, end_block: int) -> dict:
         }
 
 
+def last_block(node_url: str) -> dict:
     full_url = f'https://{node_url}/blocks/last'
     response = get(full_url)
 
@@ -67,7 +69,7 @@ def asset_distribution(node_url: str, asset_id: str) -> dict:
 def balance_asset_from_address(node_url: str, adress: str) -> dict:
     full_url = f'https://{node_url}/assets/balance/{adress}'
     response = get(full_url)
-    
+
     if response.ok:
         return {
             'status': 'ok',
