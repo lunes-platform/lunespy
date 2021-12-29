7d48c0c008398225cf2e6ffa9750836cd885ed2d
Isaque Souza <isaque.souza@lunes.io>
2021-12-29 01:05:16 +0000

    changed: new way for class Node

--- lunespy/server/__init__.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum
-
-class Node(Enum):
-    mainnet_url: str = 'https://lunesnode.lunes.io'
-    testnet_url: str = 'https://lunesnode-testnet.lunes.io'
-    mainnet_blockexplorer: str = 'https://blockexplorer.lunes.io'
-    testnet_blockexplorer: str = 'https://blockexplorer-testnet.lunes.io'
-    mainnet_total_supply: float = 150_728_537.61498705
-    testnet_total_supply: float = 800_100_000.00000000  
--- lunespy/server/nodes/__init__.py
+++ lunespy/server/nodes/__init__.py
@@ -1,5 +1,6 @@
 from requests import get
-from lunespy.server import Node
+from lunespy.server.nodes import Node
+from enum import Enum
 
 
 def all_node_conected_in_node_url(node_url: str = None) -> dict:
@@ -66,3 +67,12 @@ def version_all_lunes_node_conected(node_url: str = None) -> dict:
             'status': 'error',
             'response': response.text
         }
+
+
+class Node(Enum):
+    mainnet_url: str = 'https://lunesnode.lunes.io'
+    testnet_url: str = 'https://lunesnode-testnet.lunes.io'
+    mainnet_blockexplorer: str = 'https://blockexplorer.lunes.io'
+    testnet_blockexplorer: str = 'https://blockexplorer-testnet.lunes.io'
+    mainnet_total_supply: float = 150_728_537.61498705
+    testnet_total_supply: float = 800_100_000.00000000  
