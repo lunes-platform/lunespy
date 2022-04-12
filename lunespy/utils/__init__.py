ccc98866e232e6ab0dbf5bdf0c3068ef4ea1b023
Lucas Oliveira <lucas.oliveira@lunes.io>
2022-04-11 23:01:18 -0300

    remove: unused code

--- lunespy/utils/__init__.py
+++ lunespy/utils/__init__.py
@@ -1,43 +1,6 @@
-class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
-
 def flat_map(listOfLists):
     from itertools import chain
     return list(chain.from_iterable(listOfLists))
-
-
-def log_data(data: dict) -> None:
-    list(map(
-        lambda tuple: print(
-            f"{tuple[0]}{bcolors.OKGREEN} ─> {str(tuple[1])}{bcolors.ENDC}"
-        ),
-        data.items()
-        )
-    )
-
-
-def export_json(data: dict, name: str, path: str) -> bool:
-    import json
-    path = path.replace("//", "/")
-    full_path = f"{path}{name}.json"
-    try:
-        with open(full_path, 'w') as file:
-            file.write( json.dumps(data) )
-    except Exception as msg:
-        raise Exception(
-            bcolors.FAIL + f"[Error] File Don't Saved Because:\n└──{msg}" + bcolors.ENDC
-        )
-
-    return f"file save in {full_path}"
-
 
 # def semantic_version() -> str:
 #     from subprocess import check_output
@@ -124,24 +87,3 @@ def now() -> int:
     return int(
         time() * 1000
     )
-
-
-def lunes_to_unes(lunes: float or int) -> int:
-    return int(lunes * 10e7)
-
-
-def unes_to_lunes(unes: int) -> float:
-    return float(unes / 10e7)
-
-
-# def release():
-#     changelog(50)
-
-
-def parallel_test():
-    import subprocess
-    subprocess.run([
-        "pytest",
-        "--workers", "8",
-        "--tests-per-worker", "1"
-    ])
