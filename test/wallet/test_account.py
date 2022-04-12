8fc5a3a9959f959b0bd3aedbde863dee6ff11c86
Lucas Oliveira <lucas.oliveira@lunes.io>
2022-04-11 22:57:16 -0300

    update: test for wallet and transfer tx

--- test/wallet/test_account.py
+++ test/wallet/test_account.py
@@ -4,19 +4,21 @@ from pytest import fixture, mark
 
 
 
-
 @fixture
 def account_from_new_seed_mainnet():
-    return wallet_factory(chain=1)
+    return wallet_factory()
+
 
 @fixture
 def account_from_new_seed_testnet():
     return wallet_factory(chain=0)
 
-def validate_address_mainnet(account_from_new_seed_mainnet):
+@mark.now
+def test_validate_address_mainnet(account_from_new_seed_mainnet):
     assert validate_address(chain=1, address=account_from_new_seed_mainnet.address) == True
 
-def validate_address_testnet(account_from_new_seed_testnet):
+
+def test_validate_address_testnet(account_from_new_seed_testnet):
     assert validate_address(chain=0, address=account_from_new_seed_testnet.address) == True
 
 
@@ -25,6 +27,7 @@ def account_from_seed_mainnet():
     seed = "scrub guard swim catch range upon dawn ensure segment alpha sentence spend effort bar benefit"
     return wallet_factory(seed=seed)
 
+
 @fixture
 def account_from_seed_testnet():
     seed = "scrub guard swim catch range upon dawn ensure segment alpha sentence spend effort bar benefit"
@@ -32,9 +35,12 @@ def account_from_seed_testnet():
 
 
 def test_create_account_from_seed_mainnet(account_from_seed_mainnet):
+    assert validate_address(chain=1, address=account_from_seed_mainnet.address) == True
     assert account_from_seed_mainnet.address == "37o7aY3eZZTXmzrDa5e4Wj3Z4ZZuyV42Aaj"
 
+
 def test_create_account_from_seed_testnet(account_from_seed_testnet):
+    assert validate_address(0, account_from_seed_testnet.address) == True
     assert account_from_seed_testnet.address == "37PmyYwMGrH4uBR5V4DjCEvHGw4f2pdXW5u"
 
 
@@ -51,6 +57,7 @@ def test_create_account_from_seed_testnet(account_from_seed_testnet):
 def test_create_account_from_seed_mainnet_with_nonces_0_1_2_3_4(nonce, address):
     seed = "scrub guard swim catch range upon dawn ensure segment alpha sentence spend effort bar benefit"
     assert wallet_factory(seed=seed, nonce=nonce).address == address
+
 
 @mark.parametrize(
     "nonce, address",
@@ -72,6 +79,7 @@ def account_from_private_key_mainnet():
     private_key = "BnafXBSq1VDUdZ1nSjJoxhnQdBv2hk3o6dbV49TD1bzo"
     return wallet_factory(private_key=private_key, chain=1)
 
+
 @fixture
 def account_from_private_key_testnet():
     private_key = "BnafXBSq1VDUdZ1nSjJoxhnQdBv2hk3o6dbV49TD1bzo"
@@ -81,5 +89,7 @@ def account_from_private_key_testnet():
 def test_create_account_from_private_key_mainnet(account_from_private_key_mainnet):
     assert account_from_private_key_mainnet.address == "37o7aY3eZZTXmzrDa5e4Wj3Z4ZZuyV42Aaj"
 
+
 def test_create_account_from_private_key_testnet(account_from_private_key_testnet):
     assert account_from_private_key_testnet.address == "37PmyYwMGrH4uBR5V4DjCEvHGw4f2pdXW5u"
+
