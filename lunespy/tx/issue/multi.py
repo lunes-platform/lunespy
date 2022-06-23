b68f4e1ce49e9daa2d7c4a4c2ddd3e6cc2194829
Lucas Oliveira <olivmath@protonmail.com>
2022-06-23 17:07:33 -0300

    add: creaet multi tokens and NFT

---
+++ lunespy/tx/issue/multi.py
@@ -0,0 +1,56 @@
+
+from lunespy.tx.issue import IssueToken, issue_token_factory
+from pydantic import BaseModel, Field
+
+
+class MultiTokens(BaseModel):
+    from httpx import Response
+
+    tokens: list[IssueToken] = Field(..., description="list of issue tokens")
+
+    @property
+    def length(cls) -> int:
+        return len(cls.tokens)
+
+    def sign(cls, private_key: str):
+        cls.tokens = list(
+            map(lambda tx: tx.sign(private_key), cls.tokens)
+        )
+
+    def broadcast(cls, node: str = None) -> Response:
+        from lunespy.utils import broadcast_tx
+
+        broadcast = lambda tx: broadcast_tx(
+            node if not node == None else "https://lunesnode-testnet.lunes.io",
+            tx.dict()
+        )
+
+        return list(map(broadcast, cls.tokens))
+
+
+def issue_multiples_tokens(sender_public_key: str, tokens_list: list[dict]) -> MultiTokens:
+    return MultiTokens(tokens=[
+        issue_token_factory(
+            sender_public_key,
+            token["name"],
+            token["quantity"],
+            token["description"],
+            token["reissuable"],
+            token["decimals"]
+        )
+        for token in tokens_list
+    ])
+
+
+def mint_multiples_NFT(sender_public_key: str, tokens_list: list[dict]) -> MultiTokens:
+    def toNFT(token: dict) -> dict:
+        token["quantity"], token["decimals"] = 1, 0
+        return token
+
+    return issue_multiples_tokens(
+        sender_public_key,
+        tokens_list=[
+            toNFT(token)
+            for token in tokens_list
+        ]
+    )
